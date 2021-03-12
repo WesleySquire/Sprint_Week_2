29c3ac986704e08028de923a91d5aa7ec8053d68
EC2 Default User <ec2-user@ip-172-31-13-129.ec2.internal>
2021-03-12 19:53:34 +0000

    Initail IPO - Wesley

---
+++ NLChocolate.py
@@ -0,0 +1,19 @@
+#Sprit Week 2 - Project 1 - Python + Github
+#Create a file for default values and call it TCDef.dat.  The file will contain the Claim Number(143), the HST Rate(.15), the low per diem rate (85.00), 
+#the high per diem rate (100), the mileage rate (.10), the rental car rate (56.00)
+#Start by presenting the following menu to the user.  Validate the input to allow only 1 –5.  Set up functions for each of the choices–add a comment to describe the function only.
+#Include  at  least 2  other  functionsin  the  program  that  accept  values  as  parameters  and return a value(s) to the program.  
+#Add comment to these functions including a description, list of parameters, and a list of return values.
+
+######################################
+
+#INPUT
+#1-5 for what to do in list -
+#
+#PROCESSING
+#Do function that user inputed
+#
+
+"""Wesley Squire + Bradon Walsh
+    Sprint Week 2, Python + Github
+     March 12, 2021 - MMMM DD, YYYY"""
